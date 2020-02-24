965b511e563e54fb157391f0416b3f505e06d87c
范澳河 <fanaohe@cyai.com>
2020-02-24 17:33:03 +0800

    first:新增基础教程文件

---
+++ Go语言闭包/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+)
+
+func f1(x, y int) {
+	fmt.Println("this is f1")
+	fmt.Println(x + y)
+}
+
+func lixiang(x func(int, int), m, n int) int {
+	tmp := func() {
+		x(m, n)
+	}
+	tmp()
+
+	return 1
+
+}
+
+func main()  {
+	a := lixiang(f1, 100, 200)
+	fmt.Println(a)
+}
+
+
